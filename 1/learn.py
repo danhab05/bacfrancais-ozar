a7cf1d5975be917790735778f68154035fad9a67
Your Name <you@example.com>
2022-02-18 15:03:59 +0100

    last release

--- 1/learn.py
+++ 1/learn.py
@@ -12,16 +12,16 @@ with open('1.json', encoding='utf-8') as fh:
 
 for el in data:
     os.system("clear")
-    print(colored(el["citation"], "green"))
+    input(colored(el["citation"], "green"))
     # engine.say(el["citation"])
     # engine.runAndWait()
-    sleep(4)
+    # sleep(2)
 
-    print(colored(el["procedes"], "yellow"))
+    input(colored(el["procedes"], "yellow"))
     # engine.say(el["procedes"])
     # engine.runAndWait()
 
-    print(colored(el['analyse'], "blue"))
+    input(colored(el['analyse'], "blue"))
     # engine.say(el["analyse"])
     # engine.runAndWait()
-    sleep(6)
+    sleep(3)
