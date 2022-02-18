814fd2f8205b0e59f8eedf01b4b9a585e71c701a
Your Name <you@example.com>
2022-02-18 14:36:24 +0100

    last release

---
+++ 1/learn.py
@@ -0,0 +1,27 @@
+import os
+from time import sleep
+from termcolor import colored
+import json
+import pyttsx3
+engine = pyttsx3.init()
+voice = engine.getProperty('voices')[0]  # the french voice
+engine.setProperty('voice', voice.id)
+
+with open('1.json', encoding='utf-8') as fh:
+    data = json.load(fh)
+
+for el in data:
+    os.system("cls")
+    print(colored(el["citation"], "green"))
+    engine.say(el["citation"])
+    engine.runAndWait()
+    sleep(2)
+
+    print(colored(el["procedes"], "yellow"))
+    engine.say(el["procedes"])
+    engine.runAndWait()
+
+    print(colored(el['analyse'], "blue"))
+    engine.say(el["analyse"])
+    engine.runAndWait()
+    sleep(3)
