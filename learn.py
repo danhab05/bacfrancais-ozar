8a5635814c3f54ee03d6644402993439a2ab43eb
Your Name <you@example.com>
2022-03-13 13:41:48 +0100

    minor change

--- learn.py
+++ learn.py
@@ -1,4 +1,5 @@
 import os
+import platform
 from time import sleep
 from termcolor import colored
 import json
@@ -6,23 +7,27 @@ import json
 # engine = pyttsx3.init()
 # voice = engine.getProperty('voices')[0]  # the french voice
 # engine.setProperty('voice', voice.id)
-
-os.system("clear")
+if platform.system() == "Windows": 
+    os.system("cls") 
+else: 
+    os.system("clear")
 n = input('Entrez le numero de texte (1,2,3,4,5,7,8): ')
 with open(f'{n}/{n}.json', encoding='utf-8') as fh:
     data = json.load(fh)
 
+title = ""
 for el in data:
-    os.system("clear")
-    input(colored(el["citation"], "green"))
-    # engine.say(el["citation"])
-    # engine.runAndWait()
-
-    input(colored(el["procedes"], "yellow"))
-    # engine.say(el["procedes"])
-    # engine.runAndWait()
-
+    if platform.system() == "Windows": 
+        os.system("cls") 
+    else: 
+        os.system("clear")
+    citation = el["citation"]
+    if "MOUVEMENT" in citation:
+        title = citation
+    else:
+        print(title)
+    input(colored(citation, "green"))
+    input(colored(el["procedes"], "yellow")) 
     input(colored(el['analyse'], "blue"))
-    # engine.say(el["analyse"])
-    # engine.runAndWait()
-    # sleep(6)
+   
+   
