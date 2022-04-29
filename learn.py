a07e16c08d862f19f1a0a4aecedae5065558d301
Your Name <you@example.com>
2022-04-29 13:19:34 +0200

    aa

--- learn.py
+++ learn.py
@@ -7,27 +7,31 @@ import json
 # engine = pyttsx3.init()
 # voice = engine.getProperty('voices')[0]  # the french voice
 # engine.setProperty('voice', voice.id)
-if platform.system() == "Windows": 
-    os.system("cls") 
-else: 
-    os.system("clear")
-n = input('Entrez le numero de texte (1,2,3,4,5,7,8): ')
-with open(f'{n}/{n}.json', encoding='utf-8') as fh:
-    data = json.load(fh)
-
-title = ""
-for el in data:
-    if platform.system() == "Windows": 
-        os.system("cls") 
-    else: 
-        os.system("clear")
-    citation = el["citation"]
-    if "MOUVEMENT" in citation:
-        title = citation
+while True:
+    if platform.system() == "Windows":
+        os.system("cls")
     else:
-        print(colored(title, "red"))
-    input(colored(citation, "green"))
-    input(colored(el["procedes"], "yellow")) 
-    input(colored(el['analyse'], "blue"))
-   
-   
+        os.system("clear")
+    while True:
+        try:
+            n = input('Entrez le numero de texte (1,2,3,4,5,7,8,9,10): ')
+            with open(f'{n}/{n}.json', encoding='utf-8') as fh:
+                data = json.load(fh)
+            break
+        except Exception:
+            pass
+
+    title = ""
+    for el in data:
+        if platform.system() == "Windows":
+            os.system("cls")
+        else:
+            os.system("clear")
+        citation = el["citation"]
+        if "MOUVEMENT" in citation:
+            title = citation
+        else:
+            print(colored(title, "red"))
+        input(colored(citation, "green"))
+        input(colored(el["procedes"], "yellow"))
+        input(colored(el['analyse'], "blue"))
