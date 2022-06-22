00aa4b30e02a830ee141da3d204464f63be9b435
Your Name <you@example.com>
2022-06-22 16:18:05 +0200

    aa

---
+++ create/tempCodeRunnerFile.py
@@ -0,0 +1,24 @@
+
+import json
+from bs4 import BeautifulSoup
+lst = []
+
+with open('a.html', 'r') as f:
+    contents = f.read()
+    soup = BeautifulSoup(contents, 'lxml')
+    tr = soup.find_all("tr")
+    for td in tr:
+        dt = td.find_all("td")
+        try:
+            if "EXTRAIT" not in dt[0].text:
+                lst.append(
+                    {
+                        "citation": dt[0].text,
+                        "procedes": dt[1].text,
+                        "analyse": dt[2].text
+                    }
+                )
+        except Exception:
+            pass
+with open('m.json', 'w', encoding='utf-8') as f:
+    json.dump(lst, f, ensure_ascii=False, indent=4)
